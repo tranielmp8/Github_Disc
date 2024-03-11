15940a19b49c5337482835765448aa1d757266e0
TPride <tranielmp8@gmail.com>
2024-03-11 18:52:59 -0500

    initial commit

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+// Header
+import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+} from '@nextui-org/react';
+
+import HeaderAuth from './header-auth';
+
+export default async function Header() {
+
+  return (
+    <Navbar className='shadow mb-6'>
+      <NavbarBrand>
+        <Link href="/" className='font-bold'> Discuss </Link>
+      </NavbarBrand>
+      <NavbarContent justify='center'>
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+          <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  )
+}
