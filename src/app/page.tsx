2e475670869de424d941b63244fc1c0405d7bfc0
TPride <tranielmp8@gmail.com>
2024-03-14 05:55:28 -0500

    production ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="border shadow py-3 px-2">
         <TopicCreateForm />
         <Divider className="my-2" />
-        <h3 className="text-lg" >Topics</h3>
+        <h3 className="text-lg" >Github Topics</h3>
         <TopicList />
       </div>
     </div>
